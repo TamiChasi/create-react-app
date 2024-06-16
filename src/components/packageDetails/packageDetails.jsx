afafe394b9261779840b687f8d78a2b12825139d
Tami <tamarMa3@taxes.gov.il>
2024-06-16 09:48:39 +0300

    add transtations to pkg details

--- src/components/packageDetails/packageDetails.jsx
+++ src/components/packageDetails/packageDetails.jsx
@@ -1,11 +1,17 @@
 import React ,{useContext} from 'react'
 import { packageContext } from '../../context/packageContext'
+import { useTranslation } from 'react-i18next'
+
 
 export default function PackageDetails() {
 
+  const {t} = useTranslation()
   let packageData = useContext(packageContext);
 
   return (
-    <div>declerationNumber: {packageData.declerationNumber} | status: {packageData.status}</div>
+    <div>
+      <h3>{t ("packageDetailsTilte")} {packageData.declerationNumber}</h3>
+      <p>{t ("declerationNumber")} : {packageData.status}</p>
+    </div>
   )
 }
