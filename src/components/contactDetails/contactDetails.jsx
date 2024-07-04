ae67b976dd23caba920081244b3457b9fbfedc24
efrat tarab <e0527166813@gmail.com>
2024-07-04 12:23:45 +0300

    search css

--- src/components/contactDetails/contactDetails.jsx
+++ src/components/contactDetails/contactDetails.jsx
@@ -2,12 +2,29 @@ import React ,{useContext} from 'react'
 import { packageContext } from '../../context/packageContext'
 import { useTranslation } from 'react-i18next'
 import './contactDetails.css'
+import PackageDetails from '../packageDetails/packageDetails'
 
 
-export default function ContactDetails() {
+export default function ContactDetails({ details }) {
 
   const {t} = useTranslation()
   let {packageData, searchTime} = useContext(packageContext);
+
+
+
+
+
+  // return (
+  //   <div className='contactDetails'>
+  //     <h3>{t("caringFactor")}</h3>
+  //     <p>{details.contactDetails}</p>
+  //     <p>{details.contactPhone}</p>
+  //     <p>{t("orBySite")}</p>
+  //     <p>{details.contactSite}</p>
+  //   </div>
+  // );
+
+
 
   return (
     <div className='contactDetails'>
@@ -16,6 +33,7 @@ export default function ContactDetails() {
       <p>{packageData.contactPhone}</p>
       <p>{t ("orBySite")}</p>
       <p>{packageData.contactSite}</p>
+      
     </div>
   )
 }
