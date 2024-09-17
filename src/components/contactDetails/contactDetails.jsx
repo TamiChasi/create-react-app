8eaa9e04e6aed823e5cf5ccbfaa026bb7ffcc768
MiriamGluskinos-dev <miriamtz1315@gmail.com>
2024-09-17 11:03:43 +0300

    fix Tami's CR on API call

--- src/components/contactDetails/contactDetails.jsx
+++ src/components/contactDetails/contactDetails.jsx
@@ -10,15 +10,16 @@ export default function ContactDetails({ details }) {
 
   const {t} = useTranslation()
   let {packageData, searchTime} = useContext(packageContext);
+  var packageStatus = packageData?.CargoResult[0];
 
 
   return (
     <div className='contactDetails'>
       <h3>{t ("caringFactor")}</h3>
-      <p>{packageData?.contactDetails}</p>
-      <p>{packageData?.CargoResult[0].ContactPhoneNumber}</p>
+      <p>{packageStatus?.contactDetails}</p>
+      <p>{packageStatus?.ContactPhoneNumber}</p>
       <p>{t ("orBySite")}</p>
-      <p><Link href={packageData?.CargoResult[0]?.ContactSite}>{packageData?.CargoResult[0].ContactSite}</Link></p>
+      <p><Link href={packageStatus?.ContactSite}>{packageStatus?.ContactSite}</Link></p>
     </div>
   )
 }
