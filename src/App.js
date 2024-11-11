6daad4355b18e792214ea45f537cad02e3fcfec5
Tami Matzner <tamarma3@taxes.gov.il>
2024-11-11 08:14:04 +0200

    upgrade packages, and fix security issues

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import AppRoutes from './appRoutes';
 import './i18n/config';
 
 function App() {
+  const debug = require('debug');
+  debug.enable(''); //block all debug for security.
+
   return (
     <div className="App">
       <AppRoutes/>
